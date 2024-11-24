884382f361e973a7cf75904f97942e0ecea3c7f5
Piyush Garg <piyushgarg809@gmail.com>
2024-11-24 20:13:46 +0530

    24-11-2024

---
+++ Python/Day90.py
@@ -0,0 +1,5 @@
+print('Use the new API \n and fetch the new of daily bases as like cricket weather nd text ')
+
+print("go to http://newssapi.org/")
+
+print("And explore the various option to the build the your application")
