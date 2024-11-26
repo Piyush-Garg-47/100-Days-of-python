8c0f3ad76c8a5a45603b75c8e61ccfc28c294c54
Piyush Garg <piyushgarg809@gmail.com>
2024-11-26 18:41:15 +0530

    Exercise

---
+++ Day94(Exercise).py
@@ -0,0 +1,7 @@
+# Exercise 11 :- Drinking water reeminder 
+
+# Write a python program to reminds you to drink water at every hour or two 
+
+# you program can either beep or send a notification for a specific oprating system 
+
+print("Exercise 11 , Drink water reminder !!")
