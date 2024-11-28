92bd4e466ac8226e84d10948bd6cf016a0d60464
Piyush Garg <piyushgarg809@gmail.com>
2024-11-28 21:52:43 +0530

    last

---
+++ Day100.py
@@ -0,0 +1,5 @@
+print("about to last you have to go from here !! ")
+
+print("here is i have multiple option like a can go for machine learning or  data science or backned development")
+
+print("but i have decided i will learn pandas befor going any where")
